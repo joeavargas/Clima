a17b48ce093fb92282f1b826458cbc0e241d8497
Joe Vargas <joeavargas@outlook.com>
2021-05-16 02:52:38 -0500

    Handle permission getting user location

---
+++ Clima/WeatherVC.swift
@@ -0,0 +1,72 @@
+//
+//  WeatherVC.swift
+//  Clima
+//
+//  Created by Joe Vargas on 5/16/21.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherVC: UIViewController {
+    
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        checkLocationServices()
+        
+    }
+
+
+}
+
+//MARK: - Location Delegates and Helper Functions
+extension WeatherVC: CLLocationManagerDelegate{
+    
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            // setup location manager
+            setupLocationManager()
+            checkLocationAuthorization()
+            
+        } else {
+            // Show alert letting the user know they have to turn this on
+        }
+    }
+    
+    func checkLocationAuthorization(){
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse:
+            // do GPS location stuff
+            break
+        case .denied:
+            //TODO: Show an alert instructing user how to turn on permission
+            break
+        case.notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //TODO: Show an alert instructing user how to turn on permission
+            break
+        case .authorizedAlways:
+            break
+        default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+    }
+}
