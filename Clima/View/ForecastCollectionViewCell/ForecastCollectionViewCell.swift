b28fd27a4424958a0351d629b30a76399ff637a6
Joe Vargas <joeavargas@outlook.com>
2021-05-22 08:43:03 -0500

    ForecastCollectionViewCell designed

---
+++ Clima/View/ForecastCollectionViewCell/ForecastCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastCollectionViewCell.swift
+//  Clima
+//
+//  Created by Joe Vargas on 5/22/21.
+//
+
+import UIKit
+
+class ForecastCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
